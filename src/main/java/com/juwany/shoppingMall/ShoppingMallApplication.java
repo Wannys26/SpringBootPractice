d490fbb8341dda111030d5c47972cd5199f036a3
fkgrkyr <fkgrkyr@gmail.com>
2024-10-18 03:23:06 +0900

    스프링부트 틀

---
+++ src/main/java/com/juwany/shoppingMall/ShoppingMallApplication.java
@@ -0,0 +1,14 @@
+package com.juwany.shoppingMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingMallApplication.class, args);
+		System.out.println("안녕");
+	}
+	
+}
